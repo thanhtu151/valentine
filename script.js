e34823e95867a7060305ac856fa81b88ce747307
thanhtu151 <tu.nguyenthanh151@gmail.com>
2025-02-14 22:54:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "Yayyy!! :3<br>Chúc em có một ngày Valentine toẹt cà là vời! 💖<br>Nếu em lạnh như một cơn gió<br>Anh sẽ là mùa thu Hà Nội<br>Mát mát thay phần em...";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "Em chắc chưa",
+    "Em không kiếm được ai như anh đâu",
+    "Đừng mà công chúa của anh",
+    "Em đang làm trái tim anh tan nát đó",
+    "Quá lạnh lùng !!!",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
